8f25aca970266e87e72863b3e5adbe79f8257c7e
Wrexik <hofnfik@gmail.com>
2024-05-16 19:15:07 +0200

    new

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -3,10 +3,10 @@ import type { SiteConfig } from '$lib/types/site'
 export const site: SiteConfig = {
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
   domain: import.meta.env.URARA_SITE_DOMAIN ?? 'urara-demo.netlify.app',
-  title: 'Wrexik Blog',
+  title: 'Wrexik Web',
   subtitle: 'Home',
   lang: 'en-US',
-  description: 'Powered by Love :P',
+  description: 'Powered by Love 💜',
   author: {
     avatar: '/assets/favicon.png',
     name: 'Wrexik',
